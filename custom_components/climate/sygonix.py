b2b0ec60dc393b6be017fa9b9b696cca17df2343
Stefan Seidel <github@stefanseidel.info>
2018-01-14 23:27:28 +0100

    new version to work with BTLE to MQTT bridge

--- custom_components/climate/sygonix.py
+++ custom_components/climate/sygonix.py
@@ -10,6 +10,7 @@ https://home-assistant.io/components/climate.sygonix/
 """
 import logging
 import asyncio
+import json
 
 from datetime import (timedelta, datetime)
 
@@ -31,29 +32,20 @@ from homeassistant.const import (
     CONF_DEVICES,
     ATTR_TEMPERATURE)
 
+from homeassistant.core import callback
 import homeassistant.helpers.config_validation as cv
+import homeassistant.components.mqtt as mqtt
 
 from homeassistant.util import Throttle
 
-REQUIREMENTS = ['bluepy']
+REQUIREMENTS = ['paho-mqtt']
+DEPENDENCIES = ['mqtt']
 
 _LOGGER = logging.getLogger(__name__)
 
 STATE_AUTO_LOCKED = "auto_locked"
 STATE_MANUAL = "manual"
 STATE_MANUAL_LOCKED = "manual_locked"
-
-MODE_TO_CODE = {
-    STATE_AUTO: 0x00,
-    STATE_AUTO_LOCKED: 0x80,
-    STATE_MANUAL: 0x01,
-    STATE_MANUAL_LOCKED: 0x81 }
-CODE_TO_MODE = {
-    0x00: STATE_AUTO,
-    0x80: STATE_AUTO_LOCKED,
-    0x01: STATE_MANUAL,
-    0x81: STATE_MANUAL_LOCKED }
-
 
 
 ATTR_VENDOR_NAME = 'vendor_name'
@@ -62,10 +54,10 @@ ATTR_FIRMWARE = 'firmware'
 ATTR_VERSION = 'version'
 ATTR_TARGET = 'target_temp'
 ATTR_BATTERY = 'battery_level'
+ATTR_WINDOW = 'window_open'
 
-MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=10)
-SCAN_INTERVAL = timedelta(seconds=10)
-READ_UPDATE_FACTOR = 6
+MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=300)
+SCAN_INTERVAL = timedelta(seconds=300)
 
 DEVICE_SCHEMA = vol.Schema({
     vol.Required(CONF_MAC): cv.string,
@@ -87,8 +79,109 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
 
     add_devices(devices)
 
+class SygonixState():
+    BIT_MANUAL = 0x01
+    BIT_LOCKED = 0x80
+    BIT_WINDOW = 0x10
 
-# pylint: disable=import-error
+    def __init__(self):
+        self.temperature = None
+        self.target_temp = None
+        self.manual = None
+        self.locked = None
+        self.window = None
+        self._battery_level = None
+        self.manufacturer = None
+        self.software_rev = None
+        self.firmware_rev = None
+        self.model_no = None
+        self.name = None
+        self.last_seen = None
+        self.last_talked = None
+
+    @property
+    def mode_value(self):
+        val = 0
+        if self.manual:
+            val |= self.BIT_MANUAL
+        if self.window:
+            val |= self.BIT_WINDOW
+        if self.locked:
+            val |= self.BIT_LOCKED
+        return val
+
+    @mode_value.setter
+    def mode_value(self, value):
+        self.manual = (value & self.BIT_MANUAL) > 0
+        self.window = (value & self.BIT_WINDOW) > 0
+        self.locked = (value & self.BIT_LOCKED) > 0
+
+    @property
+    def mode_code(self):
+        if self.manual is None or self.locked is None:
+            return None
+        if self.manual:
+            if self.locked:
+                return STATE_MANUAL_LOCKED
+            else:
+                return STATE_MANUAL
+        else:
+            if self.locked:
+                return STATE_AUTO_LOCKED
+            else:
+                return STATE_AUTO
+
+    @mode_code.setter
+    def mode_code(self, value):
+        if value == STATE_MANUAL:
+            self.manual = True
+            self.locked = False
+        elif value == STATE_MANUAL_LOCKED:
+            self.manual = True
+            self.locked = True
+        elif value == STATE_AUTO:
+            self.manual = False
+            self.locked = False
+        elif value == STATE_AUTO_LOCKED:
+            self.manual = False
+            self.locked = True
+
+    @property
+    def battery_level(self):
+        return self._battery_level
+
+    @battery_level.setter
+    def battery_level(self, value):
+        if value is not None and 0 <= value <= 100:
+            self._battery_level = value
+
+    def parse_adv_msg(self, payload):
+        data = bytes(bytearray.fromhex(payload))
+        self.temperature = float(data[0])/2.0
+        self.target_temp = float(data[1])/2.0
+        self.battery_level = int(data[2])
+        self.mode_value = int(data[3])
+        self.last_seen = datetime.now()
+
+    def parse_data(self, handle, payload):
+        data = json.loads(payload)
+        if handle == '14':
+            self.model_no = bytes(data).decode('utf-8')
+        elif handle == '16':
+            self.firmware_rev = bytes(data).decode('utf-8')
+        elif handle == '18':
+            self.software_rev = bytes(data).decode('utf-8')
+        elif handle == '1a':
+            self.manufacturer = bytes(data).decode('utf-8')
+        elif handle == '41':
+            self.battery_level = data[0]
+        elif handle == '3d':
+            self.mode_value = data[0]
+        elif handle == '3f':
+            self.temperature = float(data[0])/2
+            self.target_temp = float(data[1])/2
+        self.last_talked = datetime.now()
+
 class SygonixBTThermostat(ClimateDevice):
     """Representation of a Sygonix-compatible Bluetooth thermostat."""
 
@@ -96,21 +189,11 @@ class SygonixBTThermostat(ClimateDevice):
         """Initialize the thermostat."""
         self.modes = [ STATE_AUTO, STATE_AUTO_LOCKED, STATE_MANUAL, STATE_MANUAL_LOCKED ]
 
-        self._mac = mac
+        self._mac = mac.lower()
         self._name = name
-        self._model = 1
         self._pin = pin
-        self._target = None
-        self._state = None
-        self._temp = None
-        self._vname = None
-        self._model = None
-        self._firmw = None
-        self._versn = None
-        self._batty = None
-        self._opmod = None
-        self._updates = {}
-        self._lastupdate = 0
+        self._current = SygonixState()
+        self._target = SygonixState()
 
     @property
     def available(self) -> bool:
@@ -135,12 +218,12 @@ class SygonixBTThermostat(ClimateDevice):
     @property
     def current_temperature(self):
         """Return current temperature."""
-        return self._temp
+        return self._current.temperature
 
     @property
     def target_temperature(self):
         """Return the temperature we try to reach."""
-        return self._target
+        return self._target.target_temp
 
     def set_temperature(self, **kwargs):
         """Set new target temperature."""
@@ -151,13 +234,12 @@ class SygonixBTThermostat(ClimateDevice):
             temperature = self.min_temp
         if  temperature > self.max_temp:
             temperature = self.max_temp
-        self._target = temperature
-        self._updates[0x3f] = bytes([128,int(self._target*2),128,128,128,128,128])
+        self._target.target_temp = temperature
 
     @property
     def current_operation(self):
         """Current mode."""
-        return self._opmod
+        return self._current.mode_code
 
     @property
     def operation_list(self):
@@ -166,10 +248,7 @@ class SygonixBTThermostat(ClimateDevice):
 
     def set_operation_mode(self, operation_mode):
         """Set operation mode."""
-        if not operation_mode in MODE_TO_CODE:
-            return
-        self._opmod = operation_mode
-        self._updates[0x3d] = bytes([MODE_TO_CODE[self._opmod],0,0])
+        self._target.mode_code = operation_mode
 
     @property
     def min_temp(self):
@@ -179,105 +258,107 @@ class SygonixBTThermostat(ClimateDevice):
     @property
     def max_temp(self):
         """Return the maximum temperature."""
-        return 30.0
+        return 28.0
 
+    def is_stale(self):
+        _LOGGER.info("{} last seen {} last talked {}".format(self._mac, self._current.last_seen, self._current.last_talked))
+        now = datetime.now()
+        if self._current.last_seen is not None and (now - self._current.last_seen).total_seconds() < 600:
+            return False
+        if self._current.last_talked is not None and (now - self._current.last_talked).total_seconds() < 600:
+            return False
+        return True
 
     @property
     def icon(self):
         """Return the icon to use in the frontend, if any."""
-        if self._lastupdate < -10:
+        if self.is_stale():
             return 'mdi:bluetooth-off'
-        if self._batty is None:
+        if self._current.battery_level is None:
             return 'mdi:bluetooth-off'
-        if self._batty == 100:
+        if self._current.battery_level == 100:
             return 'mdi:battery'
-        if self._batty == 0:
+        if self._current.battery_level == 0:
             return 'mdi:battery-alert'
-        if self._batty < 10:
+        if self._current.battery_level < 10:
             return 'mdi:battery-outline'
-        if 10 <= self._batty <= 99:
-            return 'mdi:battery-{}0'.format(int(self._batty/10))
+        if 10 <= self._current.battery_level <= 99:
+            return 'mdi:battery-{}0'.format(int(self._current.battery_level/10))
         return None
 
     @property
     def device_state_attributes(self):
         """Return the device specific state attributes."""
         return {
-            ATTR_VENDOR_NAME: self._vname,
-            ATTR_MODEL:       self._model,
-            ATTR_FIRMWARE:    self._firmw,
-            ATTR_VERSION:     self._versn,
-            ATTR_BATTERY:     self._batty,
-            ATTR_TARGET:      self._state,
+            ATTR_VENDOR_NAME: self._current.manufacturer,
+            ATTR_MODEL:       self._current.model_no,
+            ATTR_FIRMWARE:    self._current.firmware_rev,
+            ATTR_VERSION:     self._current.software_rev,
+            ATTR_BATTERY:     self._current.battery_level,
+            ATTR_TARGET:      self._current.target_temp,
+            ATTR_WINDOW:      self._current.window,
         }
 
-    @Throttle(MIN_TIME_BETWEEN_UPDATES)
+    def async_added_to_hass(self):
+        """Subscribe mqtt events.
+        This method must be run in the event loop and returns a coroutine.
+        """
+        @callback
+        def adv_received(topic, payload, qos):
+            """A new MQTT message has been received."""
+            self._current.parse_adv_msg(payload)
+            if self._current.target_temp is not None:
+                self._target.target_temp = self._current.target_temp
+            self.hass.async_add_job(self.async_update_ha_state())
+
+        @callback
+        def data_received(topic, payload, qos):
+            """A new MQTT message has been received."""
+            self._current.parse_data(topic.split('/')[3], payload)
+            #self._current.mode_code = self._target.mode_code
+            #self._current.target_temp = self._target.target_temp
+            self.hass.async_add_job(self.async_update_ha_state())
+
+        yield from mqtt.async_subscribe(self.hass, 'ble/{}/advertisement/ff'.format(self._mac), adv_received, 1)
+        yield from mqtt.async_subscribe(self.hass, 'ble/{}/data/+'.format(self._mac), data_received, 1)
+        cmds = { 'tries': 10,
+                 'commands': [
+                    { 'action': 'writeCharacteristic', 'handle': 0x47, 'value': [ 0, 0, 0, 0 ] }, # try PIN 000000 first
+                    { 'action': 'writeCharacteristic', 'handle': 0x47, 'value': [ int(x) for x in self._pin.to_bytes(4, byteorder = 'little') ] }, # send real/desired PIN
+                    { 'action': 'readCharacteristic', 'handle': 0x14 },
+                    { 'action': 'readCharacteristic', 'handle': 0x16 },
+                    { 'action': 'readCharacteristic', 'handle': 0x18 },
+                    { 'action': 'readCharacteristic', 'handle': 0x1a },
+                  ]
+               }
+        mqtt.async_publish(self.hass, 'ble/{}/commands'.format(self._mac), json.dumps(cmds), 1, False)
+
     def update(self):
         """Update the data from the thermostat."""
-        from bluepy.btle import Peripheral
 
-        _LOGGER.info("Update called, lastupdate = {} and updates = {}".format(self._lastupdate, self._updates))
+        _LOGGER.info("Update called {}".format(self._mac))
+        # send update request
+        cmds = { 'tries': 5,
+                 'commands': [
+                    { 'action': 'writeCharacteristic', 'handle': 0x47, 'value': [ int(x) for x in self._pin.to_bytes(4, byteorder = 'little') ] },
+                  ]
+               }
+        if self._current.mode_code != self._target.mode_code and self._target.manual is not None:
+            cmds['commands'].append({ 'action': 'writeCharacteristic', 'handle': 0x3d, 'value': [ self._target.mode_value, 0, 0 ] })
 
-        if self._updates:
-            self._lastupdate = 0
-        else:
-            self._lastupdate -= 1
+        if self._current.target_temp != self._target.target_temp and self._target.target_temp is not None:
+            cmds['commands'].append({ 'action': 'writeCharacteristic', 'handle': 0x3f, 'value': [ 128, int(self._target.target_temp * 2), 128, 128, 128, 128, 128 ] })
 
-        if self._lastupdate < 1:
-            device = None
-            try:
-                # connect to device
-                device = Peripheral(self._mac)
-                # send PIN code auth
-                device.writeCharacteristic(0x47, self._pin.to_bytes(4, byteorder = 'little'), True)
-                _LOGGER.info("Auth success for {}".format(self._mac))
+        if len(cmds['commands']) > 1:
+            now = datetime.now()
+            cmds['commands'].append({ 'action': 'writeCharacteristic', 'handle': 0x1d, 'value': [ now.minute, now.hour, now.day, now.month, now.year - 2000 ] })
+            cmds['commands'].append({ 'action': 'readCharacteristic', 'handle': 0x3d })
+            cmds['commands'].append({ 'action': 'readCharacteristic', 'handle': 0x3f })
+            cmds['commands'].append({ 'action': 'readCharacteristic', 'handle': 0x41 })
+            if self._current.model_no is None:
+                cmds['commands'].append({ 'action': 'readCharacteristic', 'handle': 0x14 })
+                cmds['commands'].append({ 'action': 'readCharacteristic', 'handle': 0x16 })
+                cmds['commands'].append({ 'action': 'readCharacteristic', 'handle': 0x18 })
+                cmds['commands'].append({ 'action': 'readCharacteristic', 'handle': 0x1a })
 
-                # set date+time
-                now = datetime.now()
-                device.writeCharacteristic(0x1d, bytes([now.minute, now.hour, now.day, now.month, now.year - 2000]))
-
-                # handle any outstanding value updates
-                for handle, values in list(self._updates.items()):
-                    device.writeCharacteristic(handle, values, True)
-                    _LOGGER.info("Updated handle {}".format(handle))
-                    del self._updates[handle]
-
-                # read current and target temp
-                data = device.readCharacteristic(0x3f)
-                self._temp = (data[0])/2
-                self._state = (data[1])/2
-
-                # if target temp is unkown to hass, use current value from device
-                if self._target is None:
-                    self._target = self._state
-
-                # Read OP-Mode
-                opmode = device.readCharacteristic(0x3d)[0]
-                if (opmode in CODE_TO_MODE):
-                    self._opmod = CODE_TO_MODE[opmode]
-                else:
-                    _LOGGER.error("OP-Mode {:02x} unknown!".format(opmode))
-
-                if self._opmod == STATE_MANUAL_LOCKED and self._state != self._target:
-                    self.set_temperature(ATTR_TEMPERATURE=self._target)
-
-                # battery value in percent
-                self._batty = device.readCharacteristic(0x41)[0]
-
-                # some string descriptors
-                if (self._vname is None):
-                    self._vname = device.readCharacteristic(0x1a).decode('utf-8')
-                if (self._model is None):
-                    self._model = device.readCharacteristic(0x14).decode('utf-8')
-                if (self._firmw is None):
-                    self._firmw = device.readCharacteristic(0x18).decode('utf-8')
-                if (self._versn is None):
-                    self._versn = device.readCharacteristic(0x16).decode('utf-8')
-
-                # successful update, schedule next in 6*10 seconds
-                self._lastupdate = READ_UPDATE_FACTOR
-            except Exception as e:
-                _LOGGER.error("Exception: %s ", str(e))
-            finally:
-                if device is not None:
-                    device.disconnect()
+            mqtt.publish(self.hass, 'ble/{}/commands'.format(self._mac), json.dumps(cmds), 1, False)
--- custom_components/switch/progtime.py
+++ custom_components/switch/progtime.py
@@ -6,6 +6,7 @@ https://home-assistant.io/components/switch.progtime/
 """
 import logging
 import platform
+import json
 from datetime import datetime
 from random import randint
 
@@ -15,8 +16,11 @@ from homeassistant.components.switch import (SwitchDevice, PLATFORM_SCHEMA)
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.script import Script
 from homeassistant.const import (CONF_MAC, CONF_PIN, CONF_NAME)
+import homeassistant.components.mqtt as mqtt
+from homeassistant.core import callback
 
-REQUIREMENTS = ['bluepy']
+REQUIREMENTS = ['paho-mqtt']
+DEPENDENCIES = ['mqtt']
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -40,12 +44,11 @@ class ProgtimeSwitch(SwitchDevice):
     """Representation of a Progtime Blue switch."""
 
     def __init__(self, mac, pin, name):
-        """Initialize the WOL switch."""
+        """Initialize the switch."""
         self._name = name
-        self._mac = mac
+        self._mac = mac.lower()
         self._pin = pin
         self._state = None
-        self.update()
 
     @property
     def should_poll(self):
@@ -55,9 +58,7 @@ class ProgtimeSwitch(SwitchDevice):
     @property
     def assumed_state(self):
         """Return if the state is based on assumptions."""
-        # Progtime Blue does NOT update the handles when the manual
-        # switch button is pressed, so the state may be wrong!
-        return True
+        return False
 
     @property
     def is_on(self):
@@ -72,44 +73,47 @@ class ProgtimeSwitch(SwitchDevice):
     def turn_on(self):
         """Turn the device on."""
         self._state = True
-        self.write_state(bytes([9]))
+        self.write_state()
         self.schedule_update_ha_state()
 
     def turn_off(self):
-        """Turn the device off."""
+        """Turn the device off if an off action is present."""
         self._state = False
-        self.write_state(bytes([1]))
+        self.write_state()
         self.schedule_update_ha_state()
 
-    def update(self):
-        """Check if device is on and update the state."""
-        self.write_state(bytes([]))
+    def async_added_to_hass(self):
+        """Subscribe mqtt events.
+        This method must be run in the event loop and returns a coroutine.
+        """
+        @callback
+        def adv_received(topic, payload, qos):
+            """A new MQTT message has been received."""
+            onoff = payload[7:8]
+            if onoff == '1':
+                self._state = False
+            if onoff == '9':
+                self._state = True
+            # 01 = remote off
+            # f9 = local on
+            # e1 = local off
+            # 19 = remote on
+            self.hass.async_add_job(self.async_update_ha_state())
 
-    def write_state(self, value):
-        from bluepy.btle import Peripheral
-        device = None
-        try:
-            # connect to device
-            device = Peripheral(self._mac)
-            # send PIN code auth
-            device.writeCharacteristic(0x3c, self._pin.to_bytes(4, byteorder = 'little'), True)
-            _LOGGER.info("Auth success for {}".format(self._mac))
+        yield from mqtt.async_subscribe(self.hass, 'ble/{}/advertisement/ff'.format(self._mac), adv_received, 1)
+        #yield from mqtt.async_subscribe(self.hass, 'ble/{}/data/+'.format(self._mac), data_received, 1)
 
-            # set date+time
-            now = datetime.now()
-            device.writeCharacteristic(0x25, bytes([now.minute, now.hour, now.day, now.month, now.year - 2000]))
-
-            # handle any outstanding value updates
-            if value:
-                device.writeCharacteristic(0x35, value, True)
-                # writing something here does a "commit"
-                device.writeCharacteristic(0x3a, bytes([randint(0,255)]))
-                _LOGGER.info("Updated switch to {}".format(value[0]))
-
-            self._state = (device.readCharacteristic(0x35)[0] & 8 > 0)
-
-        except Exception as e:
-            _LOGGER.error("Exception: %s ", str(e))
-        finally:
-            if device is not None:
-                device.disconnect()
+    def write_state(self):
+        cmds = { 'tries': 5,
+                 'commands': [
+                    { 'action': 'writeCharacteristic', 'handle': 0x3c, 'value': [ int(x) for x in self._pin.to_bytes(4, byteorder = 'little') ] },
+                  ]
+               }
+        now = datetime.now()
+        cmds['commands'].append({ 'action': 'writeCharacteristic', 'handle': 0x25, 'value': [ now.minute, now.hour, now.day, now.month, now.year - 2000 ] })
+        if self._state:
+            cmds['commands'].append({ 'action': 'writeCharacteristic', 'handle': 0x35, 'value': [ 9 ] })
+        else:
+            cmds['commands'].append({ 'action': 'writeCharacteristic', 'handle': 0x35, 'value': [ 1 ] })
+        cmds['commands'].append({ 'action': 'writeCharacteristic', 'handle': 0x3a, 'value': [ randint(0,255) ] })
+        mqtt.publish(self.hass, 'ble/{}/commands'.format(self._mac), json.dumps(cmds), 1, False)
